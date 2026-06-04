ae66f99ffa5dc47cbc5a80202267209d95ab4bf9
lucny <lucny@sspu-opava.cz>
2026-06-04 22:57:46 +0200

    01-kostra-projektu-konfigurace

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@ extensions = [
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "AGENTS.md"]
 
-html_theme = "pydata_sphinx_theme"
+html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
 html_css_files = ["css/custom.css"]
 html_js_files = ["js/interactions.js"]
